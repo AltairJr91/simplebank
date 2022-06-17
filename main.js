6118f96caaf497cd95ce6e0f76681bb44150fd5e
Altair Junior <altairjuniordev@gmail.com>
2022-06-16 23:37:57 -0300

    work in progress 0.1

---
+++ main.js
@@ -0,0 +1,5 @@
+/*Criar login
+-criar formulario de nova conta
+-criar verificaçao de segurança "email e cpf" 
+-criar cadastro de nova conta
+-criar classes e prototipos para novas contas sem repetir numeros.
