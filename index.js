57468e54dca51c93c6128a437e202b068a9ee899
Altair Junior <iris.lais.spfc@gmail.com>
2022-06-21 12:47:53 -0300

    refatorando 0.1

---
+++ index.js
@@ -0,0 +1,16 @@
+class Cliente {
+    constructor(nome , conta , agencia){
+    nome = this.nome
+    conta = this.conta
+    agencia = this.agencia
+    }
+    sacar(){
+
+    }
+    depositar(){
+
+    }
+    saldo(){
+        
+    }
+}
