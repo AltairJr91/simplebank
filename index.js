62e7f72589b65c1f50683481f947c159ab0dfa31
Altair Junior <iris.lais.spfc@gmail.com>
2022-06-23 21:28:27 -0300

    js 0.2

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 class Cliente {
-    constructor(nome , conta , agencia){
-    nome = this.nome
-    conta = this.conta
-    agencia = this.agencia
+    constructor(nome , conta , agencia, saldo){
+    this.nome = nome;
+    this.conta = conta;
+    this.agencia = agencia;
+    this.saldo = saldo;
     }
-    sacar(){
+    sacar(valor){
+        if (this.saldo >= valor){
+        this.saldo -= valor;}
+    }
+    depositar(valor){
+        if(valor >0){
+        this.saldo += valor;
+        }
 
     }
-    depositar(){
 
-    }
-    saldo(){
-        
-    }
 }
+
+const cliente01 = new Cliente ('usuario', 1001472 , 1001,);
+cliente01.saldo = 500
+console.log(cliente01);
+cliente01.sacar(300);
+console.log(cliente01.saldo);
+cliente01.depositar(100)
+console.log(cliente01.saldo);
